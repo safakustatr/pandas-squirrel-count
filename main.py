7c2405a765990c2b451ae42e339344b1a8793399
Safak <safakustatr@gmail.com>
2025-06-29 16:09:40 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+data = pd.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data_20250629.csv")
+
+count_black = len(data[data["Primary Fur Color"] == "Black"])
+count_gray = len(data[data["Primary Fur Color"] == "Gray"])
+count_red = len(data[data["Primary Fur Color"] == "Cinnamon"])
+
+data_dict = {
+    "Fur Color": ["Black", "Gray", "Cinnamon"],
+    "Count": [count_black, count_gray, count_red]
+}
+
+new_data = pd.DataFrame(data_dict)
+new_data.to_csv("new_data.csv")
